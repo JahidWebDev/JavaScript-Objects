553bacec01c054266af8b1cc1e4c00ab4083aa9a
JahidWebDev <webdeveloper2324@gmail.com>
2024-09-15 23:08:45 +0600

    Objects

---
+++ objects.js
@@ -0,0 +1,37 @@
+// JavaScript Objects
+/*
+const student = {
+    firstName: "jahid",
+    rollNo: 32,
+}
+
+console.log(student);
+*/
+// Output: { firstName: 'Jack', rollNo: 32 }
+
+// ===========================================
+
+// const objectName = {
+//     key1: value1,
+//     key2: value2,
+//     keyN: valueN
+// };
+
+// objectName - Name of the object.
+// key1: value1 - The first key-value pair.
+// key2: value2 - The second key-value pair.
+// keyN: valueN - The Nth key-value pair.
+
+
+const user = {
+    name: "shakil",
+    age: 23,
+    married: false,
+    greet: function(){
+        console.log("Hello there!");
+        
+    }
+}
+
+// console.log(user.age);
+// user.greet();
